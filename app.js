418c8888a8ea9c953deacacf1b0fdc26320e0715
aadi <32429170+aadiwebdev@users.noreply.github.com>
2019-01-31 18:46:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function(req, res){
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 var port = process.env.PORT || 8080;
