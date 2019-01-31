662fca4fb19ed25a07066065f135cf87d97f6e1f
Pkrishna412 <pkrishna483@gmail.com>
2019-01-31 14:48:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo"]}
 
 app.get('/', function(req, res){
-    res.sendFile('index.html');
+    res.send('Hi friends');
 });
 
 var port = process.env.PORT || 8080;
